c99ab610d970ea97f550ac27a0fa1d0e8fca48b4
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-12-09 18:05:55 +0100

    init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# nix
+# Gepetto Nix
+
+This project is a flake which provide packages and development environment to gepetto members.
+
+## NixGL
+
+This is for now mostly intended for people not running NixOS, so nixGL is used.
+Therefore, for nix-direnv, you probably need something like:
+```bash
+echo 'use flake . --impure' > .envrc
+```
---
+++ flake.nix
@@ -0,0 +1,82 @@
+{
+  inputs = {
+    ## TODO: this is the expected use
+    # we can't do that for now, and therefore we are loosing ros.cachix.org
+    #nix-ros-overlay.url = "github:lopsided98/nix-ros-overlay/master";
+    #nixpkgs.follows = "nix-ros-overlay/nixpkgs"; # IMPORTANT!!!
+
+    # ref. https://github.com/lopsided98/nix-ros-overlay/pull/538: ros pinocchio is nixpkgs pinocchio
+    nix-ros-overlay = {
+      url = "github:nim65s/nix-ros-overlay/pin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # nix-ros-overlay.inputs.nixpkgs is too old for now,
+    # patching packages which were moved to by-name is tedious,
+    # so let's switch to upstream for now.
+    # We'll have to compile / cache ROS stuff we need to gepetto.cachix.org for now.
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    ## Patches for nixpkgs
+    # init HPP v6.0.0
+    # also: hpp-fcl v2.4.5 -> coal v3.0.0
+    patch-hpp = {
+      url = "https://github.com/NixOS/nixpkgs/pull/362956.patch";
+      flake = false;
+    };
+
+    ## NixGL, for people not yet on NixOS
+    nixgl.url = "github:nix-community/nixGL";
+  };
+  outputs =
+    { nixpkgs, self, ... }@inputs:
+    inputs.nix-ros-overlay.inputs.flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import ./patched-nixpkgs.nix {
+          inherit nixpkgs system;
+          overlays = [
+            inputs.nix-ros-overlay.overlays.default
+            inputs.nixgl.overlay
+          ];
+          patches = [
+            inputs.patch-hpp
+          ];
+        };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            name = "Gepetto Main Dev Shell";
+            packages = [
+              pkgs.colcon
+              self.packages.${system}.nixgl-gepetto-gui
+              self.packages.${system}.python
+              self.packages.${system}.ros
+            ];
+          };
+        };
+        packages = {
+          nixgl-gepetto-gui =
+            with pkgs;
+            writeShellApplication {
+              name = "nixgl-gepetto-gui";
+              text = "${lib.getExe nixgl.auto.nixGLDefault} ${lib.getExe python3Packages.gepetto-gui}";
+            };
+          python = pkgs.python3.withPackages (p: [
+            p.example-robot-data
+            p.gepetto-gui
+            p.hpp
+          ]);
+          ros =
+            with pkgs.rosPackages.humble;
+            buildEnv {
+              paths = [
+                ros-core
+                turtlesim
+              ];
+            };
+        };
+      }
+    );
+}
