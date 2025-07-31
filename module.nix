f522b886a96e53ae85fcfa4df857f8926187650e
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-07-31 16:52:32 +0200

    unfree

--- module.nix
+++ module.nix
@@ -36,6 +36,7 @@
           in
           import patchedNixpkgs {
             inherit system;
+            config.allowUnfree = true;
             config.permittedInsecurePackages = [
               # SHAME
               "freeimage-3.18.0-unstable-2024-04-18"
