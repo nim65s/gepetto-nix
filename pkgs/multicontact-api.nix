3a547bfffb4f2345b3e86c98756e2395b6ba248f
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-23 17:37:48 +0200

    multicontact-api: 4.0.0 -> 4.1.0

--- pkgs/multicontact-api.nix
+++ pkgs/multicontact-api.nix
@@ -2,7 +2,6 @@
   lib,
 
   fetchFromGitHub,
-  fetchpatch,
   stdenv,
 
   # Native build inputs
@@ -13,35 +12,19 @@
   jrl-cmakemodules,
   ndcurves,
   pinocchio,
-
-  python3Packages,
-  pythonSupport ? false,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "multicontact-api";
-  version = "4.0.0";
+  version = "4.1.0";
 
   src = fetchFromGitHub {
     owner = "loco-3d";
     repo = "multicontact-api";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-c46nGfcqzMjDcmtiYptTZxXmEEc99KZuHbSsRTWrhCo=";
+    hash = "sha256-eCDC1InnDFZ1GdTLtZeuGJYOExTxaEJ/ekD0Ilnir9w=";
   };
 
-  patches = [
-    # fix darwin lib name, ref PR 78
-    (fetchpatch {
-      url = "https://github.com/loco-3d/multicontact-api/pull/78/commits/abf30c228d3e335f3a4f4e753db351a70457a508.patch";
-      hash = "sha256-f//w6wr6PncrvzbtHdb3qWGGLT77NPDY3f8TYFvRins=";
-    })
-  ];
-
-  postPatch = ''
-    substituteInPlace CMakeLists.txt --replace-fail \
-      "cmake_minimum_required(VERSION 3.10)" \
-      "cmake_minimum_required(VERSION 3.22)"
-  '';
   nativeBuildInputs = [
     cmake
   ];
@@ -49,26 +32,15 @@ stdenv.mkDerivation (finalAttrs: {
   propagatedBuildInputs = [
     boost
     jrl-cmakemodules
-  ]
-  ++ lib.optionals (!pythonSupport) [
     ndcurves
     pinocchio
-  ]
-  ++ lib.optionals pythonSupport [
-    python3Packages.boost
-    python3Packages.eigenpy
-    python3Packages.example-robot-data
-    python3Packages.ndcurves
-    python3Packages.pinocchio
-    python3Packages.pythonImportsCheckHook
   ];
 
   cmakeFlags = [
-    (lib.cmakeBool "BUILD_PYTHON_INTERFACE" pythonSupport)
+    (lib.cmakeBool "BUILD_PYTHON_INTERFACE" false)
   ];
 
   doCheck = true;
-  pythonImportsCheck = [ "multicontact_api" ];
 
   meta = {
     description = "define, store and use ContactSequence objects";
--- py-pkgs/multicontact-api.nix
+++ py-pkgs/multicontact-api.nix
@@ -1,10 +1,43 @@
 {
-  toPythonModule,
+  lib,
+
   pkgs,
+
+  toPythonModule,
+  pythonImportsCheckHook,
+
+  standalone ? true,
+
+  boost,
+  eigenpy,
+  example-robot-data,
+  ndcurves,
+  pinocchio,
 }:
 toPythonModule (
-  pkgs.multicontact-api.override {
-    inherit (pkgs) python3Packages;
-    pythonSupport = true;
-  }
+  pkgs.multicontact-api.overrideAttrs (super: {
+    pname = "py-${super.pname}";
+
+    cmakeFlags = (super.cmakeFlags or [ ]) ++ [
+      (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
+      (lib.cmakeBool "BUILD_STANDALONE_PYTHON_INTERFACE" standalone)
+    ];
+
+    propagatedBuildInputs =
+      (super.propagatedBuildInputs or [ ])
+      ++ [
+        boost
+        eigenpy
+        example-robot-data
+        ndcurves
+        pinocchio
+      ]
+      ++ lib.optional standalone pkgs.multicontact-api;
+
+    nativeCheckInputs = (super.nativeCheckInputs or [ ]) ++ [
+      pythonImportsCheckHook
+    ];
+
+    pythonImportsCheck = [ "multicontact_api" ];
+  })
 )
