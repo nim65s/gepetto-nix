b637f1ed9a6b9d6b3092dd3324f306a4e0a81c2b
Guilhem Saurel <guilhem.saurel@laas.fr>
2025-10-24 15:21:41 +0200

    py-hpp-python: 6.0.0 -> 6.1.0
    Diff: https://github.com/humanoid-path-planner/hpp-python/compare/v6.0.0...v6.1.0

--- py-pkgs/hpp-python.nix
+++ py-pkgs/hpp-python.nix
@@ -1,7 +1,6 @@
 {
   lib,
   fetchFromGitHub,
-  fetchpatch,
   buildPythonPackage,
 
   # nativeBuildInputs
@@ -30,32 +29,18 @@
 
 buildPythonPackage rec {
   pname = "hpp-python";
-  version = "6.0.0";
+  version = "6.1.0";
   pyproject = false; # built with CMake
 
   src = fetchFromGitHub {
     owner = "humanoid-path-planner";
     repo = "hpp-python";
     rev = "v${version}";
-    hash = "sha256-twBkQqc74kwKkDXWaJg25EtoiqRbTwLlNDvFeP4xs18=";
+    hash = "sha256-tuxm81XdlwZmN/Grz/qIgTVI0mLixavxsWZmS4MqH9M=";
   };
 
   prePatch = ''
     patchShebangs doc/configure.py
-  '';
-
-  patches = [
-    (fetchpatch {
-      name = "remove-hpp-core-gpl.patch";
-      url = "https://github.com/humanoid-path-planner/hpp-python/pull/48/commits/ac68fbd62ed0e3da080928dfb4dbff38a7b55d18.patch";
-      hash = "sha256-X0yft6jMgJyRm8qhLFEc/xQuIjr9Ug0BkYZOpYIh9K0=";
-    })
-  ];
-
-  postPatch = ''
-    substituteInPlace CMakeLists.txt --replace-warn \
-      "cmake_minimum_required(VERSION 3.10)" \
-      "cmake_minimum_required(VERSION 3.22)"
   '';
 
   outputs = [
@@ -92,11 +77,6 @@ buildPythonPackage rec {
     pkgs.example-robot-data
     pkgs.hpp-environments
   ];
-
-  preInstallCheck = ''
-    export ROS_PACKAGE_PATH=${pkgs.example-robot-data}/share:${pkgs.hpp-environments}/share
-    make test
-  '';
 
   pythonImportsCheck = [
     "pyhpp"
