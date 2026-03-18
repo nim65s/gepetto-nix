deb8f17ec38bdc613c0575f0a1819c56219d9fe4
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-18 09:44:48 +0100

    typo

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
         ];
         flake = {
           inherit flakeModule;
-          homeConfiguration.cpene = inputs.home-manager.lib.homeManagerConfiguration {
+          homeConfigurations.cpene = inputs.home-manager.lib.homeManagerConfiguration {
             pkgs = import inputs.nixpkgs { system = "x86_64-linux"; };
             modules = [ ./home/cpene.nix ];
           };
