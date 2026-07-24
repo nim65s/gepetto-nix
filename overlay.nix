5eaa0015b2e7c29a89c4b658d71ae8510216a41d
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-07-24 17:04:36 +0200

    overlay: add ros2nix things

--- overlay.nix
+++ overlay.nix
@@ -15,6 +15,40 @@ final: prev:
       }
     )
   ];
+
+  rosPackages = prev.rosPackages // {
+    humble = prev.rosPackages.humble.overrideScope (
+      humble-final: _humble-prev:
+      final.lib.filesystem.packagesFromDirectoryRecursive {
+        inherit (humble-final) callPackage;
+        directory = ./ros-pkgs/humble;
+      }
+    );
+
+    jazzy = prev.rosPackages.jazzy.overrideScope (
+      jazzy-final: _jazzy-prev:
+      final.lib.filesystem.packagesFromDirectoryRecursive {
+        inherit (jazzy-final) callPackage;
+        directory = ./ros-pkgs/jazzy;
+      }
+    );
+
+    kilted = prev.rosPackages.kilted.overrideScope (
+      kilted-final: _kilted-prev:
+      final.lib.filesystem.packagesFromDirectoryRecursive {
+        inherit (kilted-final) callPackage;
+        directory = ./ros-pkgs/kilted;
+      }
+    );
+
+    rolling = prev.rosPackages.rolling.overrideScope (
+      rolling-final: _rolling-prev:
+      final.lib.filesystem.packagesFromDirectoryRecursive {
+        inherit (rolling-final) callPackage;
+        directory = ./ros-pkgs/rolling;
+      }
+    );
+  };
 }
 // lib.filesystem.packagesFromDirectoryRecursive {
   inherit (final) callPackage;
