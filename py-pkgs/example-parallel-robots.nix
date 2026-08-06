36734c9e6f19e863511f701ed0de48f87ab960e1
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-08-06 10:17:25 +0200

    example-parallel-robots: add missing jrl-cmakemodules

--- py-pkgs/example-parallel-robots.nix
+++ py-pkgs/example-parallel-robots.nix
@@ -1,62 +1,68 @@
 {
   lib,
-
-  buildPythonPackage,
   fetchFromGitHub,
+  stdenv,
   python,
+  toPythonModule,
 
   # nativeBuildInputs
-  cmake,
-  pkg-config,
   pythonImportsCheckHook,
+
+  # buildInputs
+  jrl-cmakemodules,
 
   # propagatedBuildInputs
   pinocchio,
   pyyaml,
   toolbox-parallel-robots,
+
+  nix-update-script,
 }:
 
-buildPythonPackage rec {
-  pname = "example-parallel-robots";
-  version = "1.0.0";
-  pyproject = false;
+toPythonModule (
+  stdenv.mkDerivation (finalAttrs: {
+    pname = "example-parallel-robots";
+    version = "1.0.0";
 
-  src = fetchFromGitHub {
-    owner = "Gepetto";
-    repo = "example-parallel-robots";
-    tag = "v${version}";
-    hash = "sha256-AXkbA+j7w5n+zSgDLHCXkkYgV2utR259P1B4O/Th62I=";
-  };
+    src = fetchFromGitHub {
+      owner = "Gepetto";
+      repo = "example-parallel-robots";
+      tag = "v${finalAttrs.version}";
+      hash = "sha256-AXkbA+j7w5n+zSgDLHCXkkYgV2utR259P1B4O/Th62I=";
+    };
 
-  cmakeFlags = [
-    (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
-    (lib.cmakeBool "BUILD_TESTING" true)
-    # Not sure why jrl-cmakemodule fail to set this here
-    (lib.cmakeFeature "PYTHON_SITELIB" python.sitePackages)
-  ];
+    nativeBuildInputs = jrl-cmakemodules.docsNativeBuildInputs ++ [
+      pythonImportsCheckHook
+    ];
 
-  nativeBuildInputs = [
-    cmake
-    pkg-config
-    pythonImportsCheckHook
-  ];
-  propagatedBuildInputs = [
-    pinocchio
-    pyyaml
-    toolbox-parallel-robots
-  ];
+    buildInputs = [
+      jrl-cmakemodules
+    ];
 
-  # TODO: segfault ? :/
-  # preInstallCheck = ''
-  #   cmake --build . -t test
-  # '';
-  pythonImportsCheck = [ "example_parallel_robots" ];
+    propagatedBuildInputs = [
+      pinocchio
+      pyyaml
+      toolbox-parallel-robots
+    ];
 
-  meta = {
-    description = "Set of parallel robot models for general use in benchmarks and examples";
-    homepage = "https://github.com/Gepetto/example-parallel-robots";
-    license = lib.licenses.bsd3;
-    maintainers = with lib.maintainers; [ nim65s ];
-    platforms = lib.platforms.unix;
-  };
-}
+    cmakeFlags = jrl-cmakemodules.docsNativeBuildInputs ++ [
+      (lib.cmakeBool "BUILD_PYTHON_INTERFACE" true)
+      (lib.cmakeBool "BUILD_TESTING" finalAttrs.doCheck)
+      # Not sure why jrl-cmakemodule fail to set this here
+      (lib.cmakeFeature "PYTHON_SITELIB" python.sitePackages)
+    ];
+
+    doCheck = false; # TODO BAD_COMMAND ?
+    pythonImportsCheck = [ "example_parallel_robots" ];
+
+    passthru.updateScript = nix-update-script { };
+
+    meta = {
+      description = "Set of parallel robot models for general use in benchmarks and examples";
+      homepage = "https://github.com/Gepetto/example-parallel-robots";
+      license = lib.licenses.bsd3;
+      maintainers = with lib.maintainers; [ nim65s ];
+      platforms = lib.platforms.unix;
+    };
+  })
+)
