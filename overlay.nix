77b94ef0de1475fa13258e2c0b1bf62bf9b70797
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-08-25 15:41:40 +0200

    nuke tensorflow

--- overlay.nix
+++ overlay.nix
@@ -16,6 +16,24 @@ final: prev:
 
         # https://github.com/tensorflow/tensorflow/issues/102890
         tensorflow-bin = null;
+        etils = python-prev.etils.overrideAttrs (super: {
+          # need tensorflow
+          disabledTests = super.disabledTests ++ [
+            "test_use_backend"
+          ];
+          disabledTestPaths = super.disabledTestPaths ++ [
+            "etils/ecolab/array_as_img_test.py"
+            "etils/enp/array_spec_test.py"
+            "etils/enp/array_types/dtypes_test.py"
+            "etils/enp/checking_test.py"
+            "etils/enp/compat_test.py"
+            "etils/enp/geo_utils_test.py"
+            "etils/enp/interp_utils_test.py"
+            "etils/enp/linalg_test.py"
+            "etils/enp/numpy_utils_test.py"
+            "etils/etree/tree_utils_test.py"
+          ];
+        });
       }
       // lib.filesystem.packagesFromDirectoryRecursive {
         inherit (python-final) callPackage;
