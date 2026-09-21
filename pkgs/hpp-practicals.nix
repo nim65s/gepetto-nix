dce3e7954c766e662028b14625d288be58322a9e
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-09-21 19:03:49 +0200

    hpp-practicals: no more corba

--- pkgs/hpp-practicals.nix
+++ pkgs/hpp-practicals.nix
@@ -9,6 +9,7 @@
   # buildInputs
   jrl-cmakemodules,
   libsForQt5,
+  qgv,
 
   nix-update-script,
 }:
@@ -37,6 +38,7 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     jrl-cmakemodules
     libsForQt5.qtbase
+    qgv
   ];
 
   propagatedBuildInputs = [
@@ -46,7 +48,7 @@ stdenv.mkDerivation (finalAttrs: {
 
   cmakeFlags = jrl-cmakemodules.docsCmakeFlags ++ [
     (lib.cmakeBool "BUILD_TESTING" finalAttrs.doCheck)
-    (lib.cmakeBool "USE_CORBA" true)
+    (lib.cmakeBool "USE_CORBA" false)
   ];
 
   doCheck = true;
